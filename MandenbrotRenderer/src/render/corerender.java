922e8f3b82bfcdc5f86b687c17162886f5bb7d4e
aalleexxx5 <alexx4387@gmail.com>
2015-03-15 14:17:55 +0100

    started work towards optimisation of rendering speed

--- MandenbrotRenderer/src/render/corerender.java
+++ MandenbrotRenderer/src/render/corerender.java
@@ -4,12 +4,13 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.image.BufferedImage;
+import java.util.ArrayList;
 
 // Created on 03-02-2015.
 public class corerender implements ActionListener {
     double percnt;
 
-    BufferedImage render(int AREAX, int AREAY, int LOOP_LIMIT, int REEL_MAX, int REEL_MIN, int IMAG_MAX, int IMAG_MIN) {
+    BufferedImage render(int AREAX, int AREAY, int LOOP_LIMIT, int REEL_MAX, int REEL_MIN, int IMAG_MAX, int IMAG_MIN, String[] coloring) {
         BufferedImage image = new BufferedImage(AREAX, AREAY, BufferedImage.TYPE_INT_RGB);
         Graphics g = image.createGraphics();
         double dpcnt; //Delta percent
@@ -38,8 +39,7 @@ public class corerender implements ActionListener {
                 if (Math.abs(p0) < LIMIT && Math.abs(q0) < LIMIT) {
                     g.setColor(Color.black);
                 } else {
-                    //g.setColor(farve);
-                    g.setColor(AdvColorPix(count));
+                    g.setColor(AdvColorPix(count, coloring));
                 }
                 g.drawLine(j, i, j, i);
                 x = x + Dx;
@@ -50,6 +50,58 @@ public class corerender implements ActionListener {
         percnt = 100.0;
         return image;
     }
+
+    Color AdvColorPix(int count, String[] ColorIndex) {
+        int c1;
+        int c2;
+        int r;
+        int g;
+        int b;
+        int or; //old red
+        int og;
+        int ob;
+        double dr;
+        double dg;
+        double db;
+        String fg; //foreground colour
+        String bg; //background colour
+        ArrayList<Integer> ClrVal = new ArrayList<>();
+        for (int i = 0; i <= (count / 255) - 1; i++) ClrVal.add(255);
+        if (ClrVal.size() < ColorIndex.length) ClrVal.add(count % 255);
+        if (ClrVal.size() >= 2) {
+            fg = String.valueOf(ColorIndex[ClrVal.size() - 2]);
+            or = Integer.valueOf(fg.substring(0, 3));
+            og = Integer.valueOf(fg.substring(3, 6));
+            ob = Integer.valueOf(fg.substring(6, 9));
+            c2 = Integer.valueOf(String.valueOf(ClrVal.get(ClrVal.size() - 1)));
+            bg = String.valueOf(ColorIndex[ClrVal.size() - 1]);
+
+            dr = ((Integer.valueOf(bg.substring(0, 3)) - Integer.valueOf(fg.substring(0, 3))) / 256.0);
+            dg = ((Integer.valueOf(bg.substring(3, 6)) - Integer.valueOf(fg.substring(3, 6))) / 256.0);
+            db = ((Integer.valueOf(bg.substring(6, 9)) - Integer.valueOf(fg.substring(6, 9))) / 256.0);
+            r = (int) ((or) + (c2 * dr));
+            g = (int) ((og) + (c2 * dg));
+            b = (int) ((ob) + (c2 * db));
+
+        } else {
+            c1 = Integer.valueOf(String.valueOf(ClrVal.get(ClrVal.size() - 1)));
+            fg = String.valueOf(ColorIndex[ClrVal.size() - 1]);
+
+            dr = (Integer.valueOf(fg.substring(0, 3)) / 256.0);
+            dg = (Integer.valueOf(fg.substring(3, 6)) / 256.0);
+            db = (Integer.valueOf(fg.substring(6, 9)) / 256.0);
+            r = (int) (c1 * dr);
+            g = (int) (c1 * dg);
+            b = (int) (c1 * db);
+        }
+        if (r > 255 || g > 255 || b > 255 || r < 0 || g < 0 || b < 0) {
+            System.out.println(r + "," + g + "," + b);
+            return Color.black;
+        } else {
+            return new Color(r, g, b);
+        }
+    }
+
 
     @Override
     public void actionPerformed(ActionEvent e) {
